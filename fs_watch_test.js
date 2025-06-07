4ccd6ef66cedf26f1ae6e3fef91f2e076d3fe392
Yao Zongyou <yaozongyou@vip.qq.com>
2025-06-07 11:27:56 +0800

    add fs_watch_test.js

---
+++ fs_watch_test.js
@@ -0,0 +1,15 @@
+
+
+import { watch } from 'node:fs';
+watch('.', {"recursive": true}, (eventType, filename) => {
+  console.log(`event type is: ${eventType}`);
+  if (filename) {
+    console.log(`filename provided: ${filename}`);
+  } else {
+    console.log('filename not provided');
+  }
+});
+
+
+
+
